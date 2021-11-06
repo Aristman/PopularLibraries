acf6eddc177e56282640bba7fdc870e5488cbec0
Sergey Aristov <aristov775@gmail.com>
2021-11-06 21:13:48 +0300

    Release get data from new repository

--- app/src/main/java/ru/marslab/popularlibraries/data/repository/CacheRepositoryImpl.kt
+++
@@ -1,8 +0,0 @@
-package ru.marslab.popularlibraries.data.repository
-
-import ru.marslab.popularlibraries.data.room.GithubDatabase
-import ru.marslab.popularlibraries.domain.repository.CacheRepository
-
-class CacheRepositoryImpl(private val githubDB: GithubDatabase) : CacheRepository {
-
-}
---
+++ app/src/main/java/ru/marslab/popularlibraries/data/repository/GithubRepositoryWithCacheImpl.kt
@@ -0,0 +1,59 @@
+package ru.marslab.popularlibraries.data.repository
+
+import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
+import ru.marslab.popularlibraries.data.mapper.toDB
+import ru.marslab.popularlibraries.data.mapper.toDomain
+import ru.marslab.popularlibraries.data.retrofit.GithubService
+import ru.marslab.popularlibraries.data.room.GithubDatabase
+import ru.marslab.popularlibraries.domain.model.GithubRepo
+import ru.marslab.popularlibraries.domain.model.GithubUser
+import ru.marslab.popularlibraries.domain.repository.GithubRepository
+import ru.marslab.popularlibraries.domain.util.INetworkStatus
+
+class GithubRepositoryWithCacheImpl(
+    private val apiService: GithubService,
+    private val githubDB: GithubDatabase,
+    private val networkStatus: INetworkStatus
+) : GithubRepository {
+
+    override fun getUsers(): Single<List<GithubUser>> =
+        networkStatus.isOnlineSingle()
+            .flatMap { isOnline ->
+                if (isOnline) {
+                    apiService.getUsers()
+                        .flatMap { users ->
+                            Single.fromCallable {
+                                githubDB.userDao().saveUsers(users.map { it.toDB() })
+                                users.map { it.toDomain() }
+                            }
+                        }
+                } else {
+                    githubDB.userDao().getAllUsers()
+                        .map { usersDb ->
+                            usersDb.map { it.toDomain() }
+                        }
+                }
+            }
+            .subscribeOn(Schedulers.io())
+
+    override fun getUserRepos(user: GithubUser): Single<List<GithubRepo>> =
+        networkStatus.isOnlineSingle()
+            .flatMap { isOnline ->
+                if (isOnline) {
+                    apiService.getUserRepos(user.reposUrl)
+                        .flatMap { repos ->
+                            Single.fromCallable {
+                                githubDB.repoDao().saveRepos(repos.map { it.toDB(user.id) })
+                                repos.map { it.toDomain() }
+                            }
+                        }
+                } else {
+                    githubDB.repoDao().getUserRepos(user.id)
+                        .map { reposDb ->
+                            reposDb.map { it.toDomain() }
+                        }
+                }
+            }
+
+}
